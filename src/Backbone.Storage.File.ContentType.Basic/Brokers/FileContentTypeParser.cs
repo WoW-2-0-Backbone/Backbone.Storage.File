b23328c64e9f54bb4995a21574425f7a0e60ae17
Sultonbek Rakhimov <sultonbek.rakhimov@gmail.com>
2024-10-31 21:12:18 +0500

    Added basic implementation of file content type providers

---
+++ src/Backbone.Storage.File.ContentType.Basic/Brokers/FileContentTypeParser.cs
@@ -0,0 +1,34 @@
+using Backbone.Storage.File.ContentType.Abstractions.Brokers;
+using Backbone.Storage.File.ContentType.Abstractions.Models;
+
+namespace Backbone.Storage.File.ContentType.Basic.Brokers;
+
+/// <summary>
+/// Defines functionality for broker to parse and validate file content types.
+/// </summary>
+public class FileContentTypeParser(IFileContentTypeProvider fileContentTypeProvider) : IFileContentTypeParser
+{
+    public (FileContentTypeInfo? FileInfo, bool IsValid) GetFileInfo(string fileName, string contentType)
+    {
+        var fileExtension = Path.GetExtension(fileName)?.TrimStart('.') ?? string.Empty;
+
+        var matchesByContentType = !string.IsNullOrEmpty(contentType)
+            ? fileContentTypeProvider.GetByContentType(contentType)
+            : Array.Empty<FileContentTypeInfo>();
+
+        if (!string.IsNullOrEmpty(fileExtension))
+        {
+            var matchesByExtension = fileContentTypeProvider.GetByExtension(fileExtension);
+
+            var fileInfo = matchesByContentType!.Any()
+                ? matchesByExtension.Intersect(matchesByContentType).FirstOrDefault()
+                : matchesByExtension.FirstOrDefault();
+
+            return fileInfo is not null ? (fileInfo, true) : (null, false);
+        }
+
+        var contentTypeMatch = matchesByContentType.FirstOrDefault();
+        
+        return contentTypeMatch is not null ? (contentTypeMatch, true) : (null, false);
+    }
+}
